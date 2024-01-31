e4bc882661d7574201df5757a557f23dc04a2b4d
Zhengnan Lai <zhengnan@x1402c2s1b0n0h0.chn.perlmutter.nersc.gov>
2024-01-31 13:41:56 -0800

    Delete /build and configured gitignore

--- hw1/build/CMakeFiles/benchmark-blas.dir/dgemm-blas.c.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/benchmark-blas.dir/dgemm-blas.c.o: \
- /global/homes/z/zhengnan/cs5220_sp24/hw1/dgemm-blas.c \
- /usr/include/stdc-predef.h \
- /opt/cray/pe/libsci/23.12.5/GNU/12.3/x86_64/include/cblas.h \
- /usr/lib64/gcc/x86_64-suse-linux/12/include/stddef.h
--- hw1/build/CMakeFiles/benchmark-naive.dir/dgemm-naive.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/benchmark-naive.dir/dgemm-naive.c.o: \
- /global/homes/z/zhengnan/cs5220_sp24/hw1/dgemm-naive.c \
- /usr/include/stdc-predef.h
