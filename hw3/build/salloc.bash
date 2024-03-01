a05b325fb12479c6506f667c8bc676af906c0c51
Zhengnan Lai <zhengnan@login18.chn.perlmutter.nersc.gov>
2024-02-29 18:14:56 -0800

    Opt compute_force by loop unrolling

---
+++ hw3/build/salloc.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+salloc -N 1 -q interactive -t 01:00:00 --constraint gpu --account=m4341
--- hw3/gpu.cu
+++ hw3/gpu.cu
@@ -4,15 +4,16 @@
 #include <thrust/device_ptr.h>
 #include <thrust/scan.h>
 
-#define NUM_THREADS 256
-
 // Put any static global variables here that you will use throughout the simulation.
 
 int part_blks, bin_blks;
+int part_thrds, bin_thrds;
+int devId, num_procs;
 // The length of a square bin
 static double bin_length;
 // The number of bins (in one axis)
 static int num_bins;
+
 
 // The length of a square bin on gpu
 __device__ static double bin_length_gpu;
@@ -20,6 +21,7 @@ __device__ static double bin_length_gpu;
 __device__ static int num_bins_gpu;
 int* part_id_gpu;
 int* bin_id_gpu;
+int* prefix_bin_id_gpu;
 
 __device__ void apply_force_gpu(particle_t& particle, particle_t& neighbor) {
     double dx = neighbor.x - particle.x;
@@ -35,15 +37,14 @@ __device__ void apply_force_gpu(particle_t& particle, particle_t& neighbor) {
     //  very simple short-range repulsive force
     //
     double coef = (1 - cutoff / r) / r2 / mass;
-
-    // apply force to 2 particles at once
+    // apply force to both particles at once
     atomicAdd(&particle.ax, coef * dx);
     atomicAdd(&particle.ay, coef * dy);
     atomicAdd(&neighbor.ax, -coef * dx);
     atomicAdd(&neighbor.ay, -coef * dy);
 }
 
-__global__ void move_gpu(particle_t* particles, int num_parts, double size) {
+__global__ void move_gpu(particle_t* particles, int num_parts, double size, int* bin_id_gpu) {
 
     // Get thread (particle) ID
     int tid = threadIdx.x + blockIdx.x * blockDim.x;
@@ -51,6 +52,9 @@ __global__ void move_gpu(particle_t* particles, int num_parts, double size) {
         return;
 
     particle_t* p = &particles[tid];
+    int old_x = p->x / bin_length_gpu;
+    int old_y = p->y / bin_length_gpu;
+    int old = old_x + old_y * num_bins_gpu;
     //
     //  slightly simplified Velocity Verlet integration
     //  conserves energy better than explicit Euler method
@@ -71,21 +75,34 @@ __global__ void move_gpu(particle_t* particles, int num_parts, double size) {
         p->y = p->y < 0 ? -(p->y) : 2 * size - p->y;
         p->vy = -(p->vy);
     }
+
+    // Rebin
+    int x = p->x / bin_length_gpu;
+    int y = p->y / bin_length_gpu;
+    int index = x + y * num_bins_gpu;
+    if(old != index){
+        atomicSub(bin_id_gpu + old, 1);
+        atomicAdd(bin_id_gpu + index, 1);
+    }
+
+    // reset accelerations here
+    p->ax = p->ay = 0;
 }
 
 __global__ void compute_forces_bin_gpu(particle_t* parts, int num_parts, int* part_id_gpu, int* bin_id_gpu) {
     int tid = threadIdx.x + blockIdx.x * blockDim.x;
     if (tid >= num_bins_gpu * num_bins_gpu) return;
+    // Compute forces inside the same bin
     int st = tid == 0? 0 : bin_id_gpu[tid-1];
     int ed = bin_id_gpu[tid];
     for(int j = st; j < ed; j++) for(int k = j+1; k < ed; k++){
         apply_force_gpu(parts[part_id_gpu[j]], parts[part_id_gpu[k]]);
     }
+    // Compute forces for other 4 bins; loop unrolling to fasten 
     int x = tid % num_bins_gpu, y = tid / num_bins_gpu;
-    int dx = 1; int dy;
-    for(int dy = -1; dy <= 1; dy++){
-        if(x + dx < 0 || x + dx >= num_bins_gpu || y + dy < 0 || y + dy >= num_bins_gpu) continue;
-        int u = x + dx, v = y + dy;
+    // dx = 1, dy = -1
+    if(x + 1 < num_bins_gpu && y - 1 >= 0){
+        int u = x + 1, v = y - 1;
         int index1 = tid, index2 = u + v * num_bins_gpu;
         int st1 = index1 == 0? 0 : bin_id_gpu[index1-1];
         int st2 = index2 == 0? 0 : bin_id_gpu[index2-1];
@@ -95,9 +112,33 @@ __global__ void compute_forces_bin_gpu(particle_t* parts, int num_parts, int* pa
             apply_force_gpu(parts[part_id_gpu[j]], parts[part_id_gpu[k]]);
         }
     }
-    dx = 0; dy = 1; 
-    if(!(x + dx < 0 || x + dx >= num_bins_gpu || y + dy < 0 || y + dy >= num_bins_gpu)){
-        int u = x + dx, v = y + dy;
+    // dx = 1, dy = 0
+    if(x + 1 < num_bins_gpu){
+        int u = x + 1, v = y;
+        int index1 = tid, index2 = u + v * num_bins_gpu;
+        int st1 = index1 == 0? 0 : bin_id_gpu[index1-1];
+        int st2 = index2 == 0? 0 : bin_id_gpu[index2-1];
+        int ed1 = bin_id_gpu[index1];
+        int ed2 = bin_id_gpu[index2];
+        for(int j = st1; j < ed1; j++) for(int k = st2; k < ed2; k++){
+            apply_force_gpu(parts[part_id_gpu[j]], parts[part_id_gpu[k]]);
+        }
+    }
+    // dx = 1, dy = 1
+    if(x + 1 < num_bins_gpu && y + 1 < num_bins_gpu){
+        int u = x + 1, v = y + 1;
+        int index1 = tid, index2 = u + v * num_bins_gpu;
+        int st1 = index1 == 0? 0 : bin_id_gpu[index1-1];
+        int st2 = index2 == 0? 0 : bin_id_gpu[index2-1];
+        int ed1 = bin_id_gpu[index1];
+        int ed2 = bin_id_gpu[index2];
+        for(int j = st1; j < ed1; j++) for(int k = st2; k < ed2; k++){
+            apply_force_gpu(parts[part_id_gpu[j]], parts[part_id_gpu[k]]);
+        }
+    }
+    // dx = 0, dy = 1
+    if(y + 1 < num_bins_gpu){
+        int u = x, v = y + 1;
         int index1 = tid, index2 = u + v * num_bins_gpu;
         int st1 = index1 == 0? 0 : bin_id_gpu[index1-1];
         int st2 = index2 == 0? 0 : bin_id_gpu[index2-1];
@@ -116,15 +157,17 @@ __global__ void count_parts_gpu(particle_t* parts, int num_parts, int* part_id_g
     parts[tid].ax = parts[tid].ay = 0;
     int x = parts[tid].x / bin_length_gpu;
     int y = parts[tid].y / bin_length_gpu;
-    atomicAdd(bin_id_gpu + x + y * num_bins_gpu, 1);
+    int index = x + y * num_bins_gpu;
+    atomicAdd(bin_id_gpu + index, 1);
 }
 
-__global__ void insert_bins_gpu(particle_t* parts, int num_parts, int* part_id_gpu, int* bin_id_gpu) {
+__global__ void insert_bins_gpu(particle_t* parts, int num_parts, int* part_id_gpu, int* bin_id_gpu, int* prefix_bin_id_gpu) {
     int tid = threadIdx.x + blockIdx.x * blockDim.x;
     if (tid >= num_parts) return;
     int x = parts[tid].x / bin_length_gpu;
     int y = parts[tid].y / bin_length_gpu;
-    int id = atomicAdd(bin_id_gpu + x + y * num_bins_gpu, 1);
+    int index = x + y * num_bins_gpu;
+    int id = atomicAdd(prefix_bin_id_gpu + index, 1);
     part_id_gpu[id] = tid;
 }
 
@@ -134,38 +177,46 @@ void init_simulation(particle_t* parts, int num_parts, double size) {
     // parts live in GPU memory
     // Do not do any particle simulation here
     // For n = 1e6, c = 1.5 ~ 2
-    bin_length = 2 * cutoff;
+    bin_length = size / sqrt(num_parts);
     num_bins = ((int) (size / bin_length)) + 1;
-    
+
     cudaMemcpyToSymbol(bin_length_gpu, &bin_length, sizeof(double));
     cudaMemcpyToSymbol(num_bins_gpu, &num_bins, sizeof(int));
     cudaMalloc((void**)&part_id_gpu, sizeof(int) * num_parts);
-    cudaMalloc((void**)&bin_id_gpu, sizeof(int) * (num_bins * num_bins + 1));
+    cudaMalloc((void**)&bin_id_gpu, sizeof(int) * (num_bins * num_bins));
+    cudaMalloc((void**)&prefix_bin_id_gpu, sizeof(int) * (num_bins * num_bins + 1));
+    cudaMemset(bin_id_gpu, 0, sizeof(int) * (num_bins * num_bins + 1));
 
-    part_blks = (num_parts + NUM_THREADS - 1) / NUM_THREADS;
-    bin_blks = (num_bins * num_bins + NUM_THREADS - 1) / NUM_THREADS;
+    cudaGetDevice(&devId);
+    cudaDeviceGetAttribute(&num_procs, cudaDevAttrMultiProcessorCount, devId);
+    part_thrds = 512;
+    bin_thrds = 64;
+    part_blks = (num_parts + part_thrds - 1) / part_thrds;
+    bin_blks = (num_bins * num_bins + bin_thrds - 1) / bin_thrds;
+
+    // Count the number of particles in each bin
+    count_parts_gpu<<<part_blks, part_thrds>>>(parts, num_parts, part_id_gpu, bin_id_gpu);
+    // Prefix the bins
+    thrust::device_ptr<int> bin_ptr = thrust::device_pointer_cast(bin_id_gpu);
+    thrust::device_ptr<int> prefix_bin_ptr = thrust::device_pointer_cast(prefix_bin_id_gpu);
+    thrust::exclusive_scan(bin_ptr, bin_ptr + (num_bins * num_bins + 1), prefix_bin_ptr);
 }
 
 void simulate_one_step(particle_t* parts, int num_parts, double size) {
     // parts live in GPU memory
     // Rewrite this function
 
-    // Reinitialize the array
-    cudaMemset(bin_id_gpu, 0, sizeof(int) * (num_bins * num_bins + 1));
-    
-    // Count the number of particles in each bin
-    count_parts_gpu<<<part_blks, NUM_THREADS>>>(parts, num_parts, part_id_gpu, bin_id_gpu);
-    
-    // Prefix the bins
-    thrust::device_ptr<int> bin_ptr = thrust::device_pointer_cast(bin_id_gpu);
-    thrust::exclusive_scan(bin_ptr, bin_ptr + (num_bins * num_bins + 1), bin_ptr);
-
     // Asign particles into bins
-    insert_bins_gpu<<<part_blks, NUM_THREADS>>>(parts, num_parts, part_id_gpu, bin_id_gpu);
+    insert_bins_gpu<<<part_blks, part_thrds>>>(parts, num_parts, part_id_gpu, bin_id_gpu, prefix_bin_id_gpu);
 
     // Compute forces
-    compute_forces_bin_gpu<<<bin_blks, NUM_THREADS>>>(parts, num_parts, part_id_gpu, bin_id_gpu);
+    compute_forces_bin_gpu<<<bin_blks, bin_thrds>>>(parts, num_parts, part_id_gpu, prefix_bin_id_gpu);
 
     // Move particles
-    move_gpu<<<part_blks, NUM_THREADS>>>(parts, num_parts, size);
+    move_gpu<<<part_blks, part_thrds>>>(parts, num_parts, size, bin_id_gpu);
+
+    // Recompute prefix
+    thrust::device_ptr<int> bin_ptr = thrust::device_pointer_cast(bin_id_gpu);
+    thrust::device_ptr<int> prefix_bin_ptr = thrust::device_pointer_cast(prefix_bin_id_gpu);
+    thrust::exclusive_scan(bin_ptr, bin_ptr + num_bins * num_bins, prefix_bin_ptr);
 }
